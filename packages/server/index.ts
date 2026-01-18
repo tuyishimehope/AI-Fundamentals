d1d4fb1c169e3480d02d5cf4e9b88e326c67d688
tuyishimekyrie <tuyishimekyrie@gmail.com>
2026-01-18 14:00:35 +0300

    connection frontend with backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,10 @@ app.get("/", (req:Request,res:Response) =>{
     res.send(`Hello world! Your API key is ${process.env.OPENAI_API_KEY}`);
 })
 
+app.get("/api/hello", (req:Request,res:Response) =>{
+    res.json({message: "Hello from API"});
+})
+
 app.listen(port, () => {
     console.log(`Server is listening on ${port}`)
 })
