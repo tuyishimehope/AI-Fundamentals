7ede7c837b8e2fc6d57036ad8587c38f40393961
tuyishimekyrie <tuyishimekyrie@gmail.com>
2026-01-18 14:45:56 +0300

    set up prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import type { Request,Response } from "express";
+import type { Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,15 +7,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req:Request,res:Response) =>{
-    res.send(`Hello world! Your API key is ${process.env.OPENAI_API_KEY}`);
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send(`Hello world! Your API key is ${process.env.OPENAI_API_KEY}`);
+});
 
-app.get("/api/hello", (req:Request,res:Response) =>{
-    res.json({message: "Hello from API"});
-})
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello from API" });
+});
 
 app.listen(port, () => {
-    console.log(`Server is listening on ${port}`)
-})
-
+  console.log(`Server is listening on ${port}`);
+});
